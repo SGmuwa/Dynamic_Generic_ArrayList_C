cd3e01038dee8622182f68c6b790766ceb01e346
SG_Muwa <motherlode.muwa@gmail.com>
2019-07-29 15:41:32 +0400

    Убраны магические числа.

--- arrayList.h
+++ arrayList.h
@@ -17,6 +17,38 @@ typedef struct ArrayList
 } * ArrayList;
 
 
+// Перечисление возможных ошибок.
+enum ArrayList_err
+{
+	// Ошибка отсутсвует.
+	ArrayList_err_ok = 0,
+	// Указатель на лист равен NULL.
+	ArrayList_err_listNull,
+	// Указатель на массив листа равен NULL.
+	ArrayList_err_list_arrayNull,
+	// Не хватает памяти.
+	ArrayList_err_outOfMemory,
+	// Для текущей архитектуры такой размер листа невозможен.
+	ArrayList_err_lengthIsTooBig,
+	// Индекс вне диапазона списка.
+	ArrayList_err_outsideList,
+	// Копирование с области NULL запрещено.
+	ArrayList_err_copyFromNull,
+	// Копирование в область NULL не поддерживается.
+	ArrayList_err_copyToNull,
+	// Неизвестная ошибка при выполнении memcpy_s.
+	ArrayList_err_memcpy_s,
+	// Ошибка связанная с увеличением размеров списка. Проблема внутри ArrayList_prepareLength.
+	ArrayList_err_prepareLength,
+	// Ошибка связанная с присваиванием элемента. Проблема внутри ArrayList_set.
+	ArrayList_err_set,
+	// Неизвестная ошибка при выполнении memmove_s.
+	ArrayList_err_memmove_s,
+	// Ошибка связанная с добавлением элемента в конец списка.
+	ArrayList_err_addLast
+};
+
+
 /*
 Создание в оперативной памяти места для листа.
 size_t sizeType - размер одного элемента листа. Размер хранимого типа.
@@ -64,55 +96,45 @@ void ArrayList_free(ArrayList arrayList)
 Если в листе не хватает места, то выделяется дополнительная память.
 ArrayList this - Лист, который надо подготовить.
 size_t needLength - Указывает, сколько элементов нужно листу.
-Возвращает: код ошибки
-						0 - Место подготовлено.
-						1 - Нехватка памяти.
-						2 - Указатель на лист равен NULL.
-						3 - Указатель на массив листа равен NULL.
+Возвращает: код ошибки ArrayList_err_listNull, ArrayList_err_list_arrayNull, ArrayList_err_outOfMemory, ArrayList_err_ok
 */
-int ArrayList_prepareCapacity(ArrayList this, size_t needLength)
+enum ArrayList_err ArrayList_prepareCapacity(ArrayList this, size_t needLength)
 {
 	if (this == NULL)
-		return 2;
+		return ArrayList_err_listNull;
 	if (this->array == NULL)
-		return 3;
+		return ArrayList_err_list_arrayNull;
 	if (needLength < SIZE_MAX / 2) // Интересно, это условие в реальности может быть ложью?
 		needLength = needLength * 2u;
 	if (needLength > this->capacity)
 	{
 		void * buffer;
 		if ((buffer = realloc(this->array, needLength * this->sizeType)) == NULL)
-			return 1;
+			return ArrayList_err_outOfMemory;
 		this->array = buffer;
 		this->capacity = needLength;
 	}
-	return 0;
+	return ArrayList_err_ok;
 }
 
 /*
 Безопасно изменяет длинну листа.
 ArrayList this - Лист, в котором надо прибавить длинну.
 size_t addToLength - Слагаемое. Сколько нужно прибавить к текущей длине?
-Возвращает: код ошибки
-						0 - Размер листа подготовлен. Вы можете выполнить list->length += addToLength.
-						1 - Для текущей архитектуры такой размер листа невозможен.
-						2 - Нехватка памяти.
-						3 - Указатель на лист равен NULL.
-						4 - Указатель на массив листа равен NULL.
-
+Возвращает: код ошибки ArrayList_err_listNull, ArrayList_err_list_arrayNull, ArrayList_err_lengthIsTooBig, ArrayList_err_outOfMemory, ArrayList_err_ok
 */
-int ArrayList_prepareLength(ArrayList this, size_t addToLength)
+enum ArrayList_err ArrayList_prepareLength(ArrayList this, size_t addToLength)
 {
 	if (this == NULL)
-		return 3;
+		return ArrayList_err_listNull;
 	if (this->array == NULL)
-		return 4;
+		return ArrayList_err_list_arrayNull;
 	const size_t result = this->length + addToLength;
 	if (result < this->length)
-		return 1;
+		return ArrayList_err_lengthIsTooBig;
 	if (ArrayList_prepareCapacity(this, result))
-		return 2;
-	return 0;
+		return ArrayList_err_outOfMemory;
+	return ArrayList_err_ok;
 }
 
 /*
@@ -120,112 +142,96 @@ int ArrayList_prepareLength(ArrayList this, size_t addToLength)
 Эту функцию необходимо вызывать в случае, если Вы
 не собираетесь добавлять новые данные в лист.
 ArrayList this - лист, который надо очистить.
-Возвращает: код ошибки
-						0 - capacity приравнен к length.
-						1 - Нехватка памяти. realloc вернул NULL.
-						2 - Указатель на лист равен NULL.
-						3 - Указатель на массив листа равен NULL.
+Возвращает: код ошибки ArrayList_err_listNull, ArrayList_err_list_arrayNull, ArrayList_err_ok, ArrayList_err_outOfMemory
 */
-int ArrayList_removeTrash(ArrayList this)
+enum ArrayList_err ArrayList_removeTrash(ArrayList this)
 {
 	if (this == NULL)
-		return 2;
+		return ArrayList_err_listNull;
 	if (this->array == NULL)
-		return 3;
+		return ArrayList_err_list_arrayNull;
 	if (this->length == this->capacity)
-		return 0;
+		return ArrayList_err_ok;
 	void * buffer;
 	if ((buffer = realloc(this->array, this->length * this->sizeType)) == NULL)
-		return 1;
+		return ArrayList_err_outOfMemory;
 	this->array = buffer;
 	this->capacity = this->length;
-	return 0;
+	return ArrayList_err_ok;
 }
 
 /*
 Устанавливает значение элемента по индексу.
-Возвращает: код ошибки.
-			0 - Значение установлено.
-			1 - Отправленный лист имеет нулевой указатель.
-			2 - Указатель массива листа равен NULL.
-			3 - Индекс вне диапазона листа.
-			4 - Копирование с области NULL запрещено.
-			5 - memcpy_s обнаружил ошибку.
+ArrayList this - Список, в который надо установить значение элемента.
+size_t index - Номер ячейки, в которой надо заменить элемент.
+void * element - Указатель на элемент-источник, откуда должно происходить копирование.
+Возвращает: код ошибки ArrayList_err_ok, ArrayList_err_memcpy_s, ArrayList_err_copyFromNull, ArrayList_err_outsideList, ArrayList_err_list_arrayNull, ArrayList_err_listNull
 */
-int ArrayList_set(ArrayList this, size_t index, void * element)
+enum ArrayList_err ArrayList_set(ArrayList this, size_t index, void * element)
 {
 	if (this == NULL)
-		return 1;
+		return ArrayList_err_listNull;
 	if (this->array == NULL)
-		return 2;
+		return ArrayList_err_list_arrayNull;
 	if (index >= this->length)
-		return 3;
+		return ArrayList_err_outsideList;
 	if (element == NULL)
-		return 4;
+		return ArrayList_err_copyFromNull;
 #ifdef _MSC_VER
 	if (memcpy_s((char*)this->array + index * this->sizeType, (this->capacity - index) * this->sizeType, element, this->sizeType))
-		return 5;
+		return ArrayList_err_memcpy_s;
 #else
 	memcpy((char*)this->array + index * this->sizeType, element, this->sizeType);
 #endif // _MSC_VER
-	return 0;
+	return ArrayList_err_ok;
 }
 
 /*
 Получает значение элемента по индексу.
-Возвращает: код ошибки.
-			0 - Значение получено.
-			1 - Отправленный лист имеет нулевой указатель.
-			2 - Указатель массива листа равен NULL.
-			3 - Индекс вне диапазона листа.
-			4 - Копирование в область NULL запрещено.
-			5 - memcpy_s обнаружил ошибку.
+Возвращает: код ошибки ArrayList_err_ok, ArrayList_err_memcpy_s, ArrayList_err_copyToNull, ArrayList_err_outsideList, ArrayList_err_list_arrayNull, ArrayList_err_listNull
 */
-int ArrayList_get(ArrayList this, size_t index, void * output)
+enum ArrayList_err ArrayList_get(ArrayList this, size_t index, void * output)
 {
 	if (this == NULL)
-		return 1;
+		return ArrayList_err_listNull;
 	if (this->array == NULL)
-		return 2;
+		return ArrayList_err_list_arrayNull;
 	if (index >= this->length)
-		return 3;
+		return ArrayList_err_outsideList;
 	if (output == NULL)
-		return 4;
+		return ArrayList_err_copyToNull;
 #ifdef _MSC_VER
 	if (memcpy_s(output, (this->capacity - index) * this->sizeType, (char*)this->array + index * this->sizeType, this->sizeType))
-		return 5;
+		return ArrayList_err_memcpy_s;
 #else
 	memcpy(output, (char*)this->array + index * this->sizeType, this->sizeType);
 #endif // _MSC_VER
-	return 0;
+	return ArrayList_err_ok;
 }
 
 
 /*
-Добавить элемент в лист.
-Возвращает: код ошибки.
-			1 - Нехватка памяти. Операция отменена.
-			2 - Операция присваивания оказалась с ошибкой.
-			3 - Указатель на лист равен NULL.
-			4 - Указатель на массив в листе равен NULL.
-			5 - element == NULL не поддерживается.
+Добавить элемент в список в последнюю позицию.
+ArrayList this - Список, куда надо добавить элемент.
+void * element - Указатель на элемент, который надо поместить в список последним.
+Возвращает: код ошибки ArrayList_err_ok, ArrayList_err_set, ArrayList_err_prepareLength, ArrayList_err_copyFromNull, ArrayList_err_list_arrayNull, ArrayList_err_listNull
 */
-int ArrayList_addLast(ArrayList this, void * element)
+enum ArrayList_err ArrayList_addLast(ArrayList this, void * element)
 {
 	if (this == NULL)
-		return 3;
+		return ArrayList_err_listNull;
 	if (this->array == NULL)
-		return 4;
+		return ArrayList_err_list_arrayNull;
 	if (element == NULL)
-		return 5;
+		return ArrayList_err_copyFromNull;
 	if (ArrayList_prepareLength(this, 1))
-		return 1;
+		return ArrayList_err_prepareLength;
 	if (ArrayList_set(this, this->length++, element))
 	{
 		this->length--;
-		return 2;
+		return ArrayList_err_set;
 	}
-	return 0;
+	return ArrayList_err_ok;
 }
 
 /*
@@ -234,28 +240,20 @@ int ArrayList_addLast(ArrayList this, void * element)
 ArrayList this - Лист, в который надо добавить элемент.
 size_t index - Место, куда надо добавить элемент.
 void * element - Указатель на элемент, который надо добавить.
-Возвращает: код ошибки
-						0 - Элемент добавлен.
-						1 - Не удалось выделить или подготовить память под новый элемент.
-						2 - Не удалось переместить элемент (memmove_s != 0).
-						3 - Ошибка при добавлении элемента в последнюю позицию.
-						4 - Указатель на лист равен NULL.
-						5 - Указатель в листе на массив равен NULL.
-						6 - Индекс слишком велик.
-						7 - element == NULL не поддерживается.
+Возвращает: код ошибки ArrayList_err_ok, ArrayList_err_addLast, ArrayList_err_set, ArrayList_err_memmove_s, ArrayList_err_outsideList, ArrayList_err_prepareLength, ArrayList_err_copyFromNull, ArrayList_err_list_arrayNull, ArrayList_err_listNull
 */
-int ArrayList_add(ArrayList this, size_t index, void * element)
+enum ArrayList_err ArrayList_add(ArrayList this, size_t index, void * element)
 {
 	if (this == NULL)
-		return 4;
+		return ArrayList_err_listNull;
 	if (this->array == NULL)
-		return 5;
+		return ArrayList_err_list_arrayNull;
 	if (element == NULL)
-		return 7;
+		return ArrayList_err_copyFromNull;
 	if (ArrayList_prepareLength(this, 1))
-		return 1;
+		return ArrayList_err_prepareLength;
 	if (index > this->length + 1)
-		return 6;
+		return ArrayList_err_outsideList;
 	if (index < this->length)
 	{ // Передвигать последние элементы нужно только тогда, когда последующие элементы существуют.
 #ifdef _MSC_VER
@@ -264,40 +262,36 @@ int ArrayList_add(ArrayList this, size_t index, void * element)
 			(this->capacity - (index + 1)) * this->sizeType,
 			(char*)this->array + index * this->sizeType,
 			(this->length - index) * this->sizeType))
-			return 2; // error.
+			return ArrayList_err_memmove_s; // error.
 #else
 		memmove(
 			(char*)this->array + (index + 1) * this->sizeType,
 			(char*)this->array + index * this->sizeType,
 			(this->length - index) * this->sizeType);
 #endif // _MSC_VER
-		ArrayList_set(this, 2, element);
+		if (ArrayList_set(this, index, element))
+			return ArrayList_err_set;
 		this->length++;
 	}
 	else if (ArrayList_addLast(this, element))
-		return 3;
-	return 0;
+		return ArrayList_err_addLast;
+	return ArrayList_err_ok;
 }
 
 /*
 Удаляет элемент из листа по его индексу.
 ArrayList this - Лист, из которого нужно исключить элемент.
 size_t index - Номер элемента, который необходимо освободить.
-Возвращает: код ошибки
-						0 - Элемент успешно удалён.
-						1 - Указатель на лист равен NULL.
-						2 - Указатель на массив в листе равен NULL.
-						3 - index указывает на несущетсвующий элемент.
-						4 - Ошибка от memmove_s.
+Возвращает: код ошибки ArrayList_err_ok, ArrayList_err_memmove_s, ArrayList_err_outsideList, ArrayList_err_list_arrayNull, ArrayList_err_listNull
 */
-int ArrayList_remove(ArrayList this, size_t index)
+enum ArrayList_err ArrayList_remove(ArrayList this, size_t index)
 {
 	if (this == NULL)
-		return 1;
+		return ArrayList_err_listNull;
 	if (this->array == NULL)
-		return 2;
+		return ArrayList_err_list_arrayNull;
 	if (index >= this->length)
-		return 3;
+		return ArrayList_err_outsideList;
 	if (index != this->length - 1)
 	{ // Передвигать последние элементы нужно только тогда, когда последние элементы существуют.
 #ifdef _MSC_VER
@@ -306,7 +300,7 @@ int ArrayList_remove(ArrayList this, size_t index)
 			(this->capacity - index) * this->sizeType,
 			(char*)this->array + (index + 1)*this->sizeType,
 			(this->length - (index + 1)) * this->sizeType))
-			return 4; // error.
+			return ArrayList_err_memmove_s; // error.
 #else
 		memmove(
 			(char*)this->array + index * this->sizeType,
@@ -315,5 +309,5 @@ int ArrayList_remove(ArrayList this, size_t index)
 #endif // _MSC_VER
 	}
 	this->length--;
-	return 0;
+	return ArrayList_err_ok;
 }
